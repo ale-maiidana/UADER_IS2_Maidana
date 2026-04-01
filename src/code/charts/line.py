f7299297330eda8f0450655668dc7a75f58e52dc
ale-maiidana <maidanaalejandra08@gmail.com>
2026-04-01 19:55:18 -0300

    Agrego script de grafico con matplotlib

---
+++ src/code/charts/line.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Datos de ejemplo
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Crear gráfico
+plt.plot(x, y)
+
+# Etiquetas de los ejes
+plt.xlabel("Eje X")
+plt.ylabel("Eje Y")
+
+# Título del gráfico
+plt.title("Ejemplo de gráfico de línea")
+
+# Mostrar gráfico
+plt.show()
